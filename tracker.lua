08fa902bb9e7411690a37d2afc32101b1c8babbf
TimeDeos <96102701+TimeDeos@users.noreply.github.com>
2024-06-21 13:15:49 +0700

    Update tracker.lua

--- tracker.lua
+++ tracker.lua
@@ -68,7 +68,7 @@ else
         local body = httpService:JSONEncode(data)
     
         local response = request({
-            Url = "https://closed-margo-applecidervinegar-9c2dd853.koyeb.app/gato1",
+            Url = "http://192.168.1.2:5000/gato",
             Method = "POST",
             Headers = headers,
             Body = body
