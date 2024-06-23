c294d1e3f75de644e07339c3bfc74d88f5311de0
TimeDeos <96102701+TimeDeos@users.noreply.github.com>
2024-06-23 16:39:14 +0700

    Update tracker.lua

--- tracker.lua
+++ tracker.lua
@@ -1,79 +1,76 @@
--- You steal this shit you gay ass fuck
--- Gato FPS Boost
-repeat wait() until game:IsLoaded()
+local HttpService = game:GetService("HttpService")
 
--- Gems
-local gemsCount = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GetInventory"):InvokeServer().Currencies.Gems
--- Rerolls
-local rerollsCount = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GetInventory"):InvokeServer().Items["Trait Crystal"]
--- Level
-local levelCount = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GetInventory"):InvokeServer().Level
--- Username
-local playerName = game.Players.LocalPlayer.Name
+-- Edit this part
+local config = '{"actSelect":"6","Macros Selected":{"Raid":[],"Story":[],"Infinite":[],"Challenge":[]},"blackScreen":false,"Auto Claim Quests":true,"Auto Hide Name":false,"Auto Raid (Lobby)":false,"Enable Auto Join Player":false,"Webhook":{"ID":0,"Ping":false,"Result":false,"URL":""},"Ping On Secret Drop":false,"TP Lobby when RAID OPEN":false,"Auto TP Raid Open":false,"Auto Summon":false,"Auto Leave Wave":true,"Summon Gems":0,"Record Options":"Money","Summon Unit":"","Auto Raid":false,"Export File":"","Auto Start Time Delay":0,"Auto Lobby TP on Time":false,"Summon Select":"1","Black Screen":false,"Selected Macro":"b","Auto Tower":false,"Play Macro":true,"friendSelect":true,"Auto Start Game/Skip":false,"Auto Start Delay":0,"Auto Joiner Player":false,"IgnoreChallengeMap":[],"Auto Story":false,"Step Delay":3,"Ping On Selected Item":false,"Auto Leave":true,"IgnoreChallengeRewards":[],"Macro Mode Select":"Story","Auto Join Map":false,"Delete Map":true,"Auto Buy Raid":false,"Auto Story Mode":"Automatic","TP Delay":3,"Selected Raids":[],"AutoChallenge":false,"mapSelect":"FooshaVillage","AutoLeaveOnWave":31,"modeSelect":"Infinite","Auto Replay":true,"Selected Story":"FooshaVillage_Chapter1","IgnoreChallenge":[],"Auto Leave on Wave":false,"Auto Sell Wave":1,"Summon Mode":"All Gems","TP Lobby":false,"Raid Buy List":[],"Ping User":false,"Hide Key":"U","TP Lobby Time":10,"AutoStart":true,"Auto Buy Raid2":false,"Summon Banner":"Standard","Enable Auto Challenge":false,"Auto Sell Units":false,"Auto Joiner Player Select":"","FirstTime":true,"Auto Next":false,"PingSelected":false,"Auto Skip Wave":true,"Auto Hide UI on Execute":true,"Import Mode":"Link","Auto Try Reconnect":true,"Raid2 Buy List":[],"Enable Auto Story":false,"Large Window":false,"Auto Start":false,"Result Webhook":false,"AutoSellFarmsWave":1,"WebhookPingSelected":[],"FPS Boost":true,"Hide Additional Frame":false,"Record Macro":false,"difficultySelect":"Normal","Friends Only":false,"Auto Sell Farms":false,"AutoJoinMap":false,"Auto Execute":true}'
+-- local macroToImport = "https://cdn.discordapp.com/attachments/1247887586450735147/1247892049404104827/MagicArrow.txt?ex=66776e49&is=66761cc9&hm=d36f47aec4d342901157d72fe78c898cf0c9a6bb6ae7e022ed26762713a25458&"
+local macroToImport = ""
+local macroName = "MACHAP1"
+-- ( macroToImport should be a GitHub or Discord link. )
+-- Leave macroToImport blank if don't need to import
 
-local function blackScreen()
-    local player = game.Players.LocalPlayer
-    local playerGui = player:WaitForChild("PlayerGui")
-    local screenGui = Instance.new("ScreenGui")
-    screenGui.Name = "BlackScreenGui"
-    screenGui.Parent = playerGui
-    local blackFrame = Instance.new("Frame")
-    blackFrame.Size = UDim2.new(1, 0, 1, 0)
-    blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
-    blackFrame.BorderSizePixel = 0
-    blackFrame.Parent = screenGui
-    game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+local function rejoin()
+    wait(3)
+    -- game:GetService("Players").LocalPlayer.PlayerGui.PAGES.UpdatesUI:Destroy()
+    local ScreenGui = Instance.new("ScreenGui")
+    local Frame = Instance.new("Frame")
+    local TextLabel = Instance.new("TextLabel")
+
+    ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+    Frame.Parent = ScreenGui
+    Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    Frame.BorderSizePixel = 0
+    Frame.Size = UDim2.new(1, 0, 1, 0)
+
+    TextLabel.Parent = Frame
+    TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.BackgroundTransparency = 1
+    TextLabel.BorderSizePixel = 0
+    TextLabel.Position = UDim2.new(0.5, -100, 0.5, -50)
+    TextLabel.Size = UDim2.new(0, 200, 0, 100)
+    TextLabel.Font = Enum.Font.SourceSans
+    -- TextLabel.Text = "Rejoining to apply changes."
+    TextLabel.Text = "Applied config changes."
+    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextScaled = true
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+    wait(3)
+    ScreenGui:Destroy()
 end
 
-local function spawnFPSBoost()
-    local lobby = game:GetService("Workspace").Lobby
-    local keep = lobby.Build.Play
-
-    for _, child in ipairs(lobby:GetChildren()) do
-        if child ~= lobby.Build then
-            child:Destroy()
-        else
-            for _, buildChild in ipairs(child:GetChildren()) do
-                if buildChild ~= keep then
-                    buildChild:Destroy()
-                end
-            end
-        end
+local realMacroName = "Buang/AD/Macros/" .. macroName .. ".json"
+-- don't touch this one, only change macroName
+local function grabMacro(url)
+    local response = request({
+        Url = url,
+        Method = "GET",
+    })
+    
+    if response.Success then
+        writefile(realMacroName, response.Body)
+        print("Successfully imported the macro as", macroName .. ".json")
+    else
+        warn("Failed to fetch macro: " .. response.StatusMessage)
     end
-    workspace.TowerModel:Destroy()
-    workspace.SubStuff_DONT_DELETE.Leaderboards:Destroy()
-    workspace.SubStuff_DONT_DELETE.TowerOfEternity:Destroy()
-    workspace.Model:Destroy()
-    workspace.Lobby.Build.Play:GetChildren()[9]:Destroy()
-    blackScreen()
 end
 
-ID = game.PlaceId
-if ID == 17764698696 then
-    spawnFPSBoost()
+if macroToImport == "" then
+    print("No macro to import, moving onto config")
 else
-    blackScreen()
-    while wait(15) do
-        local msg = "[" .. levelCount .. "] " .. playerName .. " - <:diamond:1244316023708979271> " .. gemsCount .. " <:rr:1244982385242804304> " .. rerollsCount
-    
-        print("Sending message:", msg)  -- Debugging line
-    
-        local httpService = game:GetService("HttpService")
-        local headers = {
-            ["Content-Type"] = "application/json"
-        }
-        local data = {
-            ["content"] = msg
-        }
-        local body = httpService:JSONEncode(data)
-    
-        local response = request({
-            Url = "http://192.168.1.2:5000/gato",
-            Method = "POST",
-            Headers = headers,
-            Body = body
-        })
-        print("Sent..?")
-        print("Server response:", response)
-    end
+    grabMacro(macroToImport)
+end
+
+local configName = "Buang/AD/Settings_" .. game.Players.LocalPlayer.Name .. ".json"
+if isfile(configName) then
+    local fileContent = readfile(configName)
+    local existingData = HttpService:JSONDecode(fileContent)
+    print("========== Current Config ==========")
+    print(existingData)
+    print("========== New Config ==========")
+    writefile(configName, config)
+    print(config)
+    rejoin()
+else
+    warn("There's no config file.. something's wrong dawg")
 end
