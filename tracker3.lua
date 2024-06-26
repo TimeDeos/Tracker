f8227078884fb037865c002727c4e18f909c0edd
TimeDeos <96102701+TimeDeos@users.noreply.github.com>
2024-06-26 12:10:59 +0700

    Update tracker3.lua

--- tracker3.lua
+++ tracker3.lua
@@ -1,100 +1,30 @@
--- You steal this shit you gay ass fuck
--- Gato FPS Boost
-repeat wait() until game:IsLoaded()
+local HttpService = game:GetService("HttpService")
 
--- Gems
-local gemsCount = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GetInventory"):InvokeServer().Currencies.Gems
--- Rerolls
-local rerollsCount = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GetInventory"):InvokeServer().Items["Trait Crystal"]
--- Level
-local levelCount = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GetInventory"):InvokeServer().Level
--- Username
-local playerName = game.Players.LocalPlayer.Name
+-- Edit this part
+local macroToImport = "https://cdn.discordapp.com/attachments/1246493755477987428/1255169894723616778/CursedFighter_Wave31_1.json?ex=667cd04e&is=667b7ece&hm=6a0dd204bca08886c3796d5e329286b8a9e1b3feb5ab970e6750cdb29e7bc761&"
+local macroName = "b"
+-- ( macroToImport should be a GitHub or Discord link. )
+-- Leave macroToImport blank if you don't need to import
 
-local function hopLowServer()
-    local Http = game:GetService("HttpService")
-    local TPS = game:GetService("TeleportService")
-    local Api = "https://games.roblox.com/v1/games/"
+local realMacroName = "Buang/AD/Macros/" .. macroName .. ".json"
+-- don't touch this one, only change macroName
 
-    local _place = game.PlaceId
-    local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
-    function ListServers(cursor)
-       local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-       return Http:JSONDecode(Raw)
+local function grabMacro(url)
+    local response = request({
+        Url = url,
+        Method = "GET",
+    })
+    
+    if response.Success then
+        writefile(realMacroName, response.Body)
+        print("Successfully imported the macro as", macroName .. ".json")
+    else
+        warn("Failed to fetch macro: " .. response.StatusMessage)
     end
-
-    local Server, Next; repeat
-       local Servers = ListServers(Next)
-       Server = Servers.data[1]
-       Next = Servers.nextPageCursor
-    until Server
-
-    TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
 end
 
-local function blackScreen()
-    local player = game.Players.LocalPlayer
-    local playerGui = player:WaitForChild("PlayerGui")
-    local screenGui = Instance.new("ScreenGui")
-    screenGui.Name = "BlackScreenGui"
-    screenGui.Parent = playerGui
-    local blackFrame = Instance.new("Frame")
-    blackFrame.Size = UDim2.new(1, 0, 1, 0)
-    blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
-    blackFrame.BorderSizePixel = 0
-    blackFrame.Parent = screenGui
-    game:GetService("RunService"):Set3dRenderingEnabled(false)
-end
-
-local function spawnFPSBoost()
-    local lobby = game:GetService("Workspace").Lobby
-    local keep = lobby.Build.Play
-
-    for _, child in ipairs(lobby:GetChildren()) do
-        if child ~= lobby.Build then
-            child:Destroy()
-        else
-            for _, buildChild in ipairs(child:GetChildren()) do
-                if buildChild ~= keep then
-                    buildChild:Destroy()
-                end
-            end
-        end
-    end
-    workspace.TowerModel:Destroy()
-    workspace.SubStuff_DONT_DELETE.Leaderboards:Destroy()
-    workspace.SubStuff_DONT_DELETE.TowerOfEternity:Destroy()
-    workspace.Model:Destroy()
-    workspace.Lobby.Build.Play:GetChildren()[9]:Destroy()
-    -- blackScreen()
-end
-
-ID = game.PlaceId
-if ID == 17764698696 then
-    spawnFPSBoost()
+if macroToImport == "" then
+    print("No macro to import.")
 else
-    -- blackScreen()
-    while wait(15) do
-        local msg = "[" .. levelCount .. "] " .. playerName .. " - <:diamond:1244316023708979271> " .. gemsCount .. " <:rr:1244982385242804304> " .. rerollsCount
-    
-        print("Sending message:", msg)  -- Debugging line
-    
-        local httpService = game:GetService("HttpService")
-        local headers = {
-            ["Content-Type"] = "application/json"
-        }
-        local data = {
-            ["content"] = msg
-        }
-        local body = httpService:JSONEncode(data)
-    
-        local response = request({
-            Url = "http://192.168.1.4:5000/gato",
-            Method = "POST",
-            Headers = headers,
-            Body = body
-        })
-        print("Sent..?")
-        print("Server response:", response)
-    end
+    grabMacro(macroToImport)
 end
