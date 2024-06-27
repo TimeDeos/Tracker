e0a79fd8fe3018c255010d2199ecbe81365586d1
TimeDeos <96102701+TimeDeos@users.noreply.github.com>
2024-06-27 12:00:45 +0700

    Update tracker4.lua

--- tracker4.lua
+++ tracker4.lua
@@ -45,35 +45,12 @@ local function spawnFPSBoost()
     workspace.SubStuff_DONT_DELETE.TowerOfEternity:Destroy()
     workspace.Model:Destroy()
     workspace.Lobby.Build.Play:GetChildren()[9]:Destroy()
-    blackScreen()
+    --blackScreen()
 end
 
 ID = game.PlaceId
 if ID == 17764698696 then
     spawnFPSBoost()
 else
-    blackScreen()
-    while wait(15) do
-        local msg = "[" .. levelCount .. "] " .. playerName .. " - <:diamond:1244316023708979271> " .. gemsCount .. " <:rr:1244982385242804304> " .. rerollsCount
-    
-        print("Sending message:", msg)  -- Debugging line
-    
-        local httpService = game:GetService("HttpService")
-        local headers = {
-            ["Content-Type"] = "application/json"
-        }
-        local data = {
-            ["content"] = msg
-        }
-        local body = httpService:JSONEncode(data)
-    
-        local response = request({
-            Url = "http://192.168.1.3:5000/gato",
-            Method = "POST",
-            Headers = headers,
-            Body = body
-        })
-        print("Sent..?")
-        print("Server response:", response)
-    end
+    print("Lol")
 end
