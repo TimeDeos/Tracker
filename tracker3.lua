7211f5ebad273f85bf6b56a68fdf15c73a501314
TimeDeos <96102701+TimeDeos@users.noreply.github.com>
2024-07-02 10:07:19 +0700

    Update tracker3.lua

--- tracker3.lua
+++ tracker3.lua
@@ -1,82 +1,30 @@
--- You steal this shit you gay ass fuck
--- Gato FPS Boost
-repeat wait() until game:IsLoaded()
+local HttpService = game:GetService("HttpService")
 
+-- Edit this part
+local macroToImport = "https://cdn.discordapp.com/attachments/1246493755477987428/1257281598546640947/CursedFighter_Wave31_1.json?ex=6683d63c&is=668284bc&hm=65c776c11815ca76b1eead2e2c230d87c1aac71f90e6d5dfe07ba843ca1c3d23&"
+local macroName = "curse"
+-- ( macroToImport should be a GitHub or Discord link. )
+-- Leave macroToImport blank if you don't need to import
 
-local function hopLowServer()
-    local Http = game:GetService("HttpService")
-    local TPS = game:GetService("TeleportService")
-    local Api = "https://games.roblox.com/v1/games/"
+local realMacroName = "Buang/AD/Macros/" .. macroName .. ".json"
+-- don't touch this one, only change macroName
 
-    local _place = game.PlaceId
-    local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
-    function ListServers(cursor)
-       local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-       return Http:JSONDecode(Raw)
+local function grabMacro(url)
+    local response = request({
+        Url = url,
+        Method = "GET",
+    })
+    
+    if response.Success then
+        writefile(realMacroName, response.Body)
+        print("Successfully imported the macro as", macroName .. ".json")
+    else
+        warn("Failed to fetch macro: " .. response.StatusMessage)
     end
-
-    local Server, Next; repeat
-       local Servers = ListServers(Next)
-       Server = Servers.data[1]
-       Next = Servers.nextPageCursor
-    until Server
-
-    TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
 end
 
-local function blackScreen()
-    local player = game.Players.LocalPlayer
-    local playerGui = player:WaitForChild("PlayerGui")
-    local screenGui = Instance.new("ScreenGui")
-    screenGui.Name = "BlackScreenGui"
-    screenGui.Parent = playerGui
-    local blackFrame = Instance.new("Frame")
-    blackFrame.Size = UDim2.new(1, 0, 1, 0)
-    blackFrame.BackgroundColor3 = Color3.new(0, 0, 0)
-    blackFrame.BorderSizePixel = 0
-    blackFrame.Parent = screenGui
-    game:GetService("RunService"):Set3dRenderingEnabled(false)
-end
-
-local function spawnFPSBoost()
-    local lobby = game:GetService("Workspace").Lobby
-    local keep = lobby.Build.Play
-
-    for _, child in ipairs(lobby:GetChildren()) do
-        if child ~= lobby.Build then
-            child:Destroy()
-        else
-            for _, buildChild in ipairs(child:GetChildren()) do
-                if buildChild ~= keep then
-                    buildChild:Destroy()
-                end
-            end
-        end
-    end
-    workspace.TowerModel:Destroy()
-    workspace.SubStuff_DONT_DELETE.Leaderboards:Destroy()
-    workspace.SubStuff_DONT_DELETE.TowerOfEternity:Destroy()
-    workspace.Model:Destroy()
-    workspace.Lobby.Build.Play:GetChildren()[9]:Destroy()
-    blackScreen()
-end
-
-local ID = game.PlaceId
-if ID == 17764698696 then
-    spawnFPSBoost()
-    wait(90)
-    while true do
-        if ID == 17764698696 then
-            hopLowServer()
-            wait(10)
-        end
-    end
+if macroToImport == "" then
+    print("No macro to import.")
 else
-    blackScreen()
-    repeat task.wait() until game:IsLoaded()
-    getgenv().Hermanos_Settings = {
-        ['key'] = '9e5a6c16-283a-49db-9ccc-8002c065413a',
-        ['PC'] = 'VPS3',
-    	}
-    task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/ad-main.lua'))() end)
+    grabMacro(macroToImport)
 end
