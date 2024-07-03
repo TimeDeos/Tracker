498e40fc8131ffa049d0f67a74da9f0d437a6250
TimeDeos <96102701+TimeDeos@users.noreply.github.com>
2024-07-03 21:46:07 +0700

    Update tracker2.lua

--- tracker2.lua
+++ tracker2.lua
@@ -39,25 +39,36 @@ local function blackScreen()
 end
 
 local function spawnFPSBoost()
+    if updatesUI then
+        updatesUI:Destroy()
+    else
+        print("Updates log thing not found")
+    end
+    spawnPlatform()
+    wait(2)
     local lobby = game:GetService("Workspace").Lobby
-    local keep = lobby.Build.Play
-
+    local keepPlay = lobby.Build.Play
+    local keepPortal = lobby.Build.Portal
+    
+    -- Iterate through all children of lobby
     for _, child in ipairs(lobby:GetChildren()) do
+        -- Check if the child is not the instance to keep
         if child ~= lobby.Build then
             child:Destroy()
         else
+            -- If the child is Build, iterate through its children
             for _, buildChild in ipairs(child:GetChildren()) do
-                if buildChild ~= keep then
+                if buildChild ~= keepPlay and buildChild ~= keepPortal then
                     buildChild:Destroy()
                 end
             end
         end
     end
-    workspace.TowerModel:Destroy()
+    
+    workspace.MirrorBuilding:Destroy()
     workspace.SubStuff_DONT_DELETE.Leaderboards:Destroy()
     workspace.SubStuff_DONT_DELETE.TowerOfEternity:Destroy()
     workspace.Model:Destroy()
-    workspace.Lobby.Build.Play:GetChildren()[9]:Destroy()
     blackScreen()
 end
 
